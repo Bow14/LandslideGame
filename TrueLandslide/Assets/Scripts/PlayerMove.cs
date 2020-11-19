622b29beaae30f544365b613a7a2d5572a59e41b
Tanner <thebow25@gmail.com>
2020-11-19 02:01:05 -0700

    Added force and fixedupdate and messed with physics.

---
+++ TrueLandslide/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+//tutorial script to learn new gaming designs
+
+	public Rigidbody rb;
+	// Use this for initialization
+	
+	
+	// Update is called once per frame
+	void FixedUpdate () //fixedupdate is used for physics properties
+	{ 
+		rb.AddForce(0, 0, 2000 * Time.deltaTime);// time. delta time is used so the game speed and such doesnt run off frames of the pc
+	}
+}
