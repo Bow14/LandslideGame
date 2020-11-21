018c78d09dbe47f76b700603a42dccdfaa09999f
Tanner <thebow25@gmail.com>
2020-11-20 20:14:22 -0700

    Finished playermovement

--- TrueLandslide/Assets/Scripts/PlayerMove.cs
+++ TrueLandslide/Assets/Scripts/PlayerMove.cs
@@ -7,12 +7,22 @@ public class PlayerMove : MonoBehaviour
 //tutorial script to learn new gaming designs
 
 	public Rigidbody rb;
-	// Use this for initialization
-	
+
+	public float forwardForce = 2000f;
+	public float sidwaysForce = 500f;
 	
 	// Update is called once per frame
 	void FixedUpdate () //fixedupdate is used for physics properties
 	{ 
-		rb.AddForce(0, 0, 2000 * Time.deltaTime);// time. delta time is used so the game speed and such doesnt run off frames of the pc
+		rb.AddForce(0, 0, forwardForce * Time.deltaTime);// time. delta time is used so the game speed and such doesnt run off frames of the pc
+
+		if (Input.GetKey("d"))
+		{
+			rb.AddForce(sidwaysForce * Time.deltaTime, 0,0);
+		}
+		if (Input.GetKey("a"))
+		{
+			rb.AddForce(-sidwaysForce * Time.deltaTime, 0,0);
+		}
 	}
 }
