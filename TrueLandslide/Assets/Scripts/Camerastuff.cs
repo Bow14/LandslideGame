1a2451c9427ecd3e6813a3bac80b3f0e1197771a
Tanner <thebow25@gmail.com>
2020-11-24 02:25:22 -0700

    Seeing a new way to do camera work tutorial

---
+++ TrueLandslide/Assets/Scripts/Camerastuff.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerastuff : MonoBehaviour
+{
+
+	public Transform player;
+	public Vector3 offset; //Vector three covers three floats used for position
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = player.position + offset; //camera will be equal to the players position 
+	}
+}
