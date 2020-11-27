7406f002f1d10b3888f35e33b45957196483269a
Tanner <thebow25@gmail.com>
2020-11-27 03:46:16 -0700

    Collision and stopping movement.

---
+++ TrueLandslide/Assets/Scripts/Collsion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collsion : MonoBehaviour
+{
+	public PlayerMove movement;
+	 void OnCollisionEnter(Collision collisionInfo)
+	{
+		if (collisionInfo.collider.tag == "Obstacle")
+		{
+			movement.enabled = false;
+		}
+		
+	}
+}
